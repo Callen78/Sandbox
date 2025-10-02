b80585c5cebad8121a7f49c0a47bfe96124cbbb1
Callen78 <carlallenjr87@gmail.com>
2025-10-01 21:55:30 -0600

    Messing around

--- Pages/Node_Files/server.js
+++ Pages/Node_Files/server.js
@@ -114,8 +114,8 @@ app.post('/subscribe', async (req, res) => {
       const infoSub = await transporter.sendMail({
         from: process.env.FROM_EMAIL || process.env.SMTP_USER || 'no-reply@example.com',
         to: email,
-        subject: 'Thanks for subscribing to Carl\'s Closet',
-        text: `Thanks for subscribing to Carl's Closet updates. We'll keep you posted!`,
+        subject: 'Wife of mine thank you',
+        text: `Its works thanks for subscribing. Love ya!`,
       });
       console.log('Confirmation email send result:', infoSub);
     } catch (err) {
